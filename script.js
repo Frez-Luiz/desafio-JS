db5c2c9af2de74d901ae5a84c7250623d3b624d2
Luiz Felipe Frez <luiz.frez.santos@escola.pr.gov.br>
2025-11-12 23:46:06 +0000

    tudo ok

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const lanterna = document.getElementById("lanterna");
 
 function acender(){
-    lanterna.src = "ligado.png";
-    document.body.style.background = "#ffeb3b22"
+    lanterna.src= "ligado.png";
+    document.body.style.background = "#ffea3085"
 }
 
 function apagar(){
     lanterna.src = "desligado.png";
-    document.body.style.background = "#222"
+    document.body.style.background = "#303030ff"
 }
 function automatico(){
-    lanterna.src = "ligado.png";
-    document.body.style.background = "#ffeb3b22"
+    let estado =0;
+    intervalo = setInterval(()=>{
+        if(estado === 0) acender();
+        else if(estado === 1) apagar();
+        estado = (estado + 1) % 3;
+    }, 350)
 }
+function parar(){
+    clearInterval(intervalo);
+    limpar();
+}
+
+btnAcender.onclick = acender;
+btnApagar.onclick = apagar;
+btnAuto.onclick = automatico;
+btnParar.onclick = parar;
